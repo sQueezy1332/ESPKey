7a09f840b7edb2fdf97e270537eb33102edf5a0d
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2024-11-23 03:16:42 +0300

    Update ESPKey.ino

--- ESPKey.ino
+++ ESPKey.ino
@@ -9,57 +9,33 @@ void setup() {
 	DEBUGLN("Chip ID: 0x" + String(ESP.getChipId(), HEX));
 	//while (true) { yield(); }
 #endif // DEBUG_ENABLE
-	led_blink();
-	pinMode(PIN_D0, OUTPUT_OPEN_DRAIN); digitalWrite(PIN_D0, HIGH);
-	pinMode(PIN_D1, OUTPUT_OPEN_DRAIN); digitalWrite(PIN_D1, HIGH);
+	pinMode(PIN_D0, OUTPUT_OPEN_DRAIN); dWrite(PIN_D0, HIGH);
+	pinMode(PIN_D1, OUTPUT_OPEN_DRAIN); dWrite(PIN_D1, HIGH);
 	pinMode(PIN_MODE, INPUT_PULLUP);
 	pinMode(PIN_CONF_RESET, INPUT_PULLUP);
-	if (dRead(PIN_MODE)) {
-		attachInterrupt(digitalPinToInterrupt(PIN_D0), reader1_D0_trigger, FALLING);
-		attachInterrupt(digitalPinToInterrupt(PIN_D1), reader1_D1_trigger, FALLING);
+	delay(1000); led_blink();
+	if (dRead(PIN_MODE)) {		//wiegand mode
+		attachInterrupt((PIN_D0), reader1_D0_trigger, FALLING);
+		attachInterrupt((PIN_D1), reader1_D1_trigger, FALLING);
 	} else {
-		onewire_mode = true;
-		attachInterrupt(digitalPinToInterrupt(PIN_D0), onewire_presence, FALLING);
+		if (slave.waitReset(1) || slave.waitReset(1)) 
+		{ onewire_mode = FALLING; } 
+		else { onewire_mode = RISING; }
+		attachInterrupt(PIN_D0, onewire_presence, onewire_mode);
 	}
 	pinMode(PIN_MODE, INPUT);
-	attachInterrupt(digitalPinToInterrupt(PIN_CONF_RESET), resetConfig, CHANGE);
+	attachInterrupt((PIN_CONF_RESET), resetConfig, CHANGE);
 	//attachInterrupt(digitalPinToInterrupt(PIN_LED), auxChange, CHANGE);
 	// Set Hostname.
-	//String dhcp_hostname(HOSTNAME);
-	//dhcp_hostname += String(ESP.getChipId(), HEX);
-	WiFi.hostname(HOSTNAME);
-	//DEBUGLN("Hostname: " + dhcp_hostname);
+	String dhcp_hostname(HOSTNAME);
+	dhcp_hostname += String(ESP.getChipId(), HEX);
+	WiFi.hostname(dhcp_hostname);
+	DEBUGLN("Hostname: " + dhcp_hostname);
 
 	if (!SPIFFS.begin()) {
 		DEBUGLN(F("Failed to mount file system"));
 		return;
-	} else {
-#ifdef ESP8266
-		Dir root = SPIFFS.openDir("/");
-#else
-		File root = SPIFFS.open("/");
-#endif
-		if (root.isDirectory()) {
-			String fileName;
-			size_t fileSize;
-#ifdef ESP8266
-			while (root.next()) {
-				fileName = root.fileName();
-				fileSize = root.fileSize();
-#else
-			for (File file = root.openNextFile(); file; file = root.openNextFile()) {
-				if (file.isDirectory()) continue;
-				fileName = file.name();
-				fileSize = file.fileSize();
-
-#endif // ESP8266
-					// This is a dirty hack to deal with readers which don't pull LED up to 5V
-				if (fileName == "/auth.txt") detachInterrupt((digitalPinToInterrupt(PIN_LED)));
-				DEBUGF("FS File: %s, size: %u bytes\n", fileName.c_str(), fileSize/*formatBytes(file.fileSize()).c_str()*/);
-
-			}
-		}
-	}
+	} //else led_detach();
 	append_log(F("Starting up!"));
 
 	// If a log.txt exists, use ap_ssid=ESPKey-<chipid> instead of the default ESPKey-config
@@ -70,31 +46,29 @@ void setup() {
 	if (!loadConfig()) {
 		DEBUGLN(F("No configuration information available.  Using defaults."));
 	}
-	if (!wifi_sta_init())
-		if (ap_enable) {
+	if (!wifi_sta_init()) {
+		/*if (ap_enable) {
 			DEBUGLN(F("Can not connect to WiFi station. Going into AP mode."));
 			// Go into software AP mode.
 			WiFi.mode(WIFI_AP);
 			WiFi.softAPConfig(ap_ip, ap_ip, IPAddress(255, 255, 255, 0));
-			WiFi.softAP(ap_ssid, ap_psk, 0, ap_hidden);
+			WiFi.softAP(ap_ssid, ap_psk, 11, ap_hidden);
 
 			DEBUG(F("IP address: "));
 			DEBUGLN(WiFi.softAPIP());
 		} else {
 			DEBUGLN(F("Can not connect to WiFi station. Bummer."));
 			WiFi.mode(WIFI_OFF);
-		}
+		}*/
+	}
 // Start OTA server.
-	ArduinoOTA.setHostname(/*dhcp_hostname.c_str()*/ap_ssid.c_str());
+	ArduinoOTA.setHostname(/*dhcp_hostname.c_str()*/mDNShost.c_str());
 	ArduinoOTA.setPassword(ota_password.c_str());
 	ArduinoOTA.begin();
-	if (MDNS.begin(mDNShost)) {
-		DEBUGLN("Open http://" + String(mDNShost) + ".local/");
-	} else { DEBUGLN("Error setting up MDNS responder!"); }
+	if (MDNS.begin(mDNShost)) { DEBUGLN("Open http://" + mDNShost + ".local/"); } else { DEBUGLN("Error setting up MDNS responder!"); }
 	server_init();
 	syslog(log_name + F(" starting up!"));
 }
-
 void loop() {
 	// Check for card reader data
 	if (onewire_mode && presence_flag) {
@@ -103,7 +77,7 @@ void loop() {
 			return;
 		};
 	}
-	if (reader_count && (uS - reader_last > 5000 /*|| millis() < 10*/)) {
+	if (reader_count > CARD_LEN && (uS - reader_last > 5000 /*|| millis() < 10*/)) {
 		//fix_reader_string();
 		noInterrupts();
 		reader_string = String(reader_code, HEX); reader_string += ':' + reader_string += reader_count;
